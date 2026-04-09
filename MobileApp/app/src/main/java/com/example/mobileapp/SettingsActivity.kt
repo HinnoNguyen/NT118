6e428c82baaf5966a3405dbf2d90d815d91cc129
Hinno <23520588@gm.uit.edu.vn>
2026-04-10 02:53:53 +0700

    setting

---
+++ MobileApp/app/src/main/java/com/example/mobileapp/SettingsActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mobileapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        setupEdgeToEdge()
+        setupUI()
+    }
+
+    private fun setupEdgeToEdge() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun setupUI() {
+        // Handle logout or navigation if needed
+    }
+}
